75c0a8ebc0db6d21dd8b90dba60bb720f35745d2
Andrei Muntean <andreymuntean2004@gmail.com>
2022-07-07 17:16:35 +0300

    fix: rename the function so it matches FC req

--- pages/scaneaza-qr.jsx
+++ pages/scaneaza-qr.jsx
@@ -1,4 +1,4 @@
-const scaneazaQr = () => {
+const ScaneazaQr = () => {
   return (
     <div>
       
@@ -6,4 +6,4 @@ const scaneazaQr = () => {
   );
 }
 
-export default scaneazaQr;
+export default ScaneazaQr;
