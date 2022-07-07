e5a7f3ec5c2edcf18d115a083d163de313003cec
Andrei Muntean <andreymuntean2004@gmail.com>
2022-07-07 17:03:46 +0300

    feat: add loader

---
+++ components/Loader.jsx
@@ -0,0 +1,11 @@
+function Loader() {
+	return (
+		<>
+			<div className="min-w-full min-h-screen bg-background flex items-center justify-center xl:items-center">
+				<h1 className="font-nunito text-white text-2xl">Se încarcă...</h1>
+			</div>
+		</>
+	);
+}
+
+export default Loader;
