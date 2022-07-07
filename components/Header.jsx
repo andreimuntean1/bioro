136bf1cb62369b04d59fe10e0fd0628f3913b68e
Andrei Muntean <andreymuntean2004@gmail.com>
2022-07-07 17:03:16 +0300

    feat: header is more dynamic based on user state

--- components/Header.jsx
+++ components/Header.jsx
@@ -3,34 +3,55 @@ import { faQrcode, faUser } from "@fortawesome/free-solid-svg-icons";
 import Link from "next/link";
 
 const Header = (props) => {
-  const { isLoggedIn } = props;
+	const { isLoggedIn, buttonText } = props;
 
-  if(isLoggedIn) {
-    return (
-      <div className="sticky top-0 font-montserrat font-bold bg-middleBackground flex justify-between items-center p-4 w-full">
-        <Link href="/" passHref><a className="text-white text-lg">bio<span className="text-accent1">RO</span></a></Link>
-        <div className="flex gap-3">
-          <div className="bg-lightBackground text-accent2 hover:bg-accent2 hover:text-middleBackground transition-colors duration-300 p-3 rounded-full cursor-pointer">
-            <Link href="/scaneaza-qr" passHref>
-              <FontAwesomeIcon icon={faQrcode} className="w-4" />
-            </Link>
-          </div>
-          <div className="bg-lightBackground text-accent2 hover:bg-accent2 hover:text-middleBackground transition-colors duration-300 p-3 rounded-full cursor-pointer">
-            <Link href="/profilul-meu" passHref>
-              <FontAwesomeIcon icon={faUser} className="w-4" />
-            </Link>
-          </div>
-        </div>
-      </div>
-    );
-  } else {
-    return (
-      <div className="sticky top-0 font-montserrat font-bold bg-middleBackground flex justify-between items-center p-4 w-full">
-        <Link href="/" passHref><a className="text-white text-lg">bio<span className="text-accent1">RO</span></a></Link>
-        <Link href="/creeaza-cont" passHref><a className="bg-lightBackground text-accent2 hover:bg-accent2 hover:text-middleBackground transition-colors duration-300 px-4 py-2 rounded-full cursor-pointer">Creează cont</a></Link>
-      </div>
-    );
-  }
-}
+	if (isLoggedIn) {
+		return (
+			<div className="sticky top-0 font-montserrat font-bold bg-middleBackground flex justify-between items-center p-4 w-full">
+				<Link href="/" passHref>
+					<a className="text-white text-lg">
+						bio<span className="text-accent1">RO</span>
+					</a>
+				</Link>
+				<div className="flex gap-3">
+					<Link href="/scaneaza-qr" passHref>
+						<div className="bg-lightBackground text-accent2 hover:bg-accent2 hover:text-middleBackground transition-colors duration-300 p-3 rounded-full cursor-pointer">
+							<FontAwesomeIcon icon={faQrcode} className="w-4" />
+						</div>
+					</Link>
+					<Link href="/profilul-meu" passHref>
+						<div className="bg-lightBackground text-accent2 hover:bg-accent2 hover:text-middleBackground transition-colors duration-300 p-3 rounded-full cursor-pointer">
+							<FontAwesomeIcon icon={faUser} className="w-4" />
+						</div>
+					</Link>
+				</div>
+			</div>
+		);
+	} else {
+		return (
+			<div className="sticky top-0 font-montserrat font-bold bg-middleBackground flex justify-between items-center p-4 w-full">
+				<Link href="/" passHref>
+					<a className="text-white text-lg">
+						bio<span className="text-accent1">RO</span>
+					</a>
+				</Link>
+				<Link
+					href={
+						!buttonText
+							? "/creeaza-cont"
+							: buttonText === "Conectează-te"
+							? "/conecteaza-te"
+							: "#"
+					}
+					passHref
+				>
+					<a className="bg-lightBackground text-accent2 hover:bg-accent2 hover:text-middleBackground transition-colors duration-300 px-4 py-2 rounded-full cursor-pointer">
+						{buttonText || "Creează cont"}
+					</a>
+				</Link>
+			</div>
+		);
+	}
+};
 
 export default Header;
