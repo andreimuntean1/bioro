bd1a49e41666daa2f0f57e4eccf46449fece72fb
Andrei Muntean <andreymuntean2004@gmail.com>
2022-07-07 17:15:01 +0300

    feat: add authentication

---
+++ pages/conecteaza-te.jsx
@@ -0,0 +1,123 @@
+import { faGoogle } from "@fortawesome/free-brands-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { auth, googleProvider } from "database";
+import { signInWithEmailAndPassword, signInWithPopup } from "firebase/auth";
+import Head from "next/head";
+import Link from "next/link";
+import { useState } from "react";
+import Header from "../components/Header";
+import { useRouter } from "next/router";
+import { useAuth } from "hooks/useAuth";
+import Loader from "components/Loader";
+
+const ConecteazaTe = () => {
+	const [email, setEmail] = useState("")
+	const [password, setPassword] = useState("")
+
+	const [error, setError] = useState("")
+  const emailValidation = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+	
+	const {pending, isSignedIn} = useAuth();
+	const router = useRouter();
+
+	const login = (e) => {
+		e.preventDefault()
+
+		if(!email){
+      setError("După calculele noastre, email-ul tău lipsește aici")
+      return
+    }
+    if(!emailValidation.test(email)) {
+      setError("Introdu un email valid")
+      return
+    }
+    if(!password) {
+      setError("Nu te prea poți conecta la cont fără o parolă😅")
+      return
+    }
+
+		signInWithEmailAndPassword(auth, email, password)
+			.then(() => {
+				router.push('/')
+			})
+			.catch((err) => {
+        console.log(`Error ${err.code}: ${err.message}`)
+      })
+	}
+
+	const loginWithGoogle = () => {
+		signInWithPopup(auth, googleProvider).catch((err) => {
+			console.log(`Error ${err.code}: ${err.message}`);
+		});
+	};
+	
+	if(pending) {
+		return <Loader />
+	}
+
+	if(isSignedIn) {
+		router.push('/')
+	}
+
+	return (
+		<>
+			<Head>
+				<title>bioRO - conectează-te</title>
+			</Head>
+
+			<div className="min-w-full min-h-screen bg-background flex flex-col xl:items-center">
+				<Header isLoggedIn={isSignedIn} />
+				<div className="flex flex-col md:items-center md:mt-6 p-8 max-w-md md:max-w-full xl:max-w-6xl">
+					<h1 className="font-montserrat text-white text-3xl md:text-4xl md:text-center mt-2">
+						Bine ai venit înapoi
+					</h1>
+					<form className="mt-12 flex flex-col md:items-center w-full max-w-sm">
+						<input
+							type="text"
+							placeholder="Email"
+							className="outline-none border-none bg-lightBackground text-white font-nunito placeholder:text-opacity-60 focus:text-opacity-100 px-5 py-3 w-full mt-6 rounded-full"
+							value={email}
+							onChange={(e) => setEmail(e.target.value)}
+						/>
+						<input
+							type="password"
+							placeholder="Parola"
+							className="outline-none border-none bg-lightBackground text-white font-nunito placeholder:text-opacity-60 focus:text-opacity-100 px-5 py-3 w-full mt-6 rounded-full"
+							value={password}
+							onChange={(e) => setPassword(e.target.value)}
+						/>
+            <p className="font-nunito text-red-500 mt-3">{error}</p>
+						<button className="bg-lightBackground font-montserrat text-accent2 hover:bg-accent2 hover:text-middleBackground transition-colors duration-300 px-6 py-2 mt-10 rounded-xl cursor-pointer" onClick={login}>
+							Conectează-te
+						</button>
+					</form>
+					<div>
+						<div className="flex items-center gap-3 w-full my-8">
+							<hr className="bg-white w-full" />
+							<h6 className="font-nunito text-white text-opacity-75 uppercase text-sm">
+								sau
+							</h6>
+							<hr className="bg-white w-full" />
+						</div>
+						<button className="bg-lightBackground font-nunito font-semibold text-accent2 hover:bg-accent2 hover:text-middleBackground transition-colors duration-300 px-6 py-2 mt-6 flex items-center rounded-xl cursor-pointer" onClick={loginWithGoogle}>
+							<FontAwesomeIcon icon={faGoogle} className="w-5 mr-4" />
+							Conectează-te cu Google
+						</button>
+					</div>
+					<div className="mt-24">
+						<h1 className="font-montserrat text-white text-3xl md:text-4xl md:text-center mt-2 mb-8">
+							Ești nou pe aici?
+						</h1>
+						<Link href="/creeaza-cont" passHref>
+							<a className="bg-lightBackground font-montserrat text-accent2 hover:bg-accent2 hover:text-middleBackground transition-colors duration-300 px-6 py-3 rounded-xl cursor-pointer">
+								Creează un cont
+							</a>
+						</Link>
+					</div>
+				</div>
+			</div>
+		</>
+	);
+};
+
+export default ConecteazaTe;
--- pages/profilul-meu.jsx
+++ pages/profilul-meu.jsx
@@ -1,9 +1,157 @@
-const profilulMeu = () => {
-  return (
-    <div>
-      
-    </div>
-  );
-}
+import Header from "components/Header";
+import { auth } from "database";
+import Head from "next/head";
+import Image from "next/image";
+import { useRouter } from "next/router";
+import { useAuth } from "hooks/useAuth";
+import { signOut, updateProfile } from "firebase/auth";
+import Loader from "components/Loader";
+import { useEffect, useRef, useState } from "react";
+import { useClickOutside } from "hooks/useClickOutside";
+	
+const ProfilulMeu = () => {
+	const { pending, isSignedIn, user } = useAuth();
+	const router = useRouter();
+	const [email, setEmail] = useState("");
+	const [name, setName] = useState("");
+	const [error, setError] = useState("");
+  const emailValidation = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+	const [popupVisibility, setPopupVisibility] = useState(false);
+	const popup = useRef(null);
 
-export default profilulMeu;
+	const logOut = () => {
+		signOut(auth);
+		router.push("/");
+	};
+
+	const updateUser = () => {
+		setError("")
+
+		if(!name) {
+      setError("Se pare că ai uitat să-ți introduci numele")
+      return
+    }
+    if (!name.split(" ").length > 1) {
+      setError("Te rog adaugă atât prenumele cât și numele")
+      return
+    }
+    if((name.split(" ").map(word => word[0] === word[0].toUpperCase())).some(v => v == false)) {
+      setError("Fiecare nume trebuie să înceapă cu majusculă")
+      return
+    }
+    if(!email){
+      setError("După calculele noastre, email-ul tău lipsește aici")
+      return
+    }
+    if(!emailValidation.test(email)) {
+      setError("Introdu un email valid")
+      return
+    }
+
+		if(user)
+			updateProfile(user, {
+				displayName: name,
+				email,
+			}).then(() => {
+				setPopupVisibility(false);
+			})
+	}
+
+	useClickOutside(popup, () => setPopupVisibility(false));
+
+	useEffect(() => {
+		if (user) {
+			setEmail(user.email);
+			setName(user.displayName);
+		}
+	}, [user]);
+
+	if (pending) {
+		return <Loader />;
+	}
+
+	if (!isSignedIn) {
+		router.push("/conecteaza-te");
+	}
+
+
+	return (
+		<>
+			<Head>
+				<title>bioRO - profilul meu</title>
+			</Head>
+
+			{popupVisibility && (
+				<div className="w-full min-h-screen absolute top-0 left-0 bg-background bg-opacity-80 flex items-center justify-center z-20">
+					<div className=" bg-middleBackground p-6 m-8 rounded-xl" ref={popup}>
+						<h3 className="font-montserrat text-white text-2xl mb-3">
+							Editează datele
+						</h3>
+						<form className="flex flex-col gap-4">
+							<input
+								type="text"
+								className="outline-none border-none bg-lightBackground text-white font-nunito placeholder:text-opacity-60 focus:text-opacity-100 px-5 py-2 w-full rounded-full"
+								defaultValue={name}
+								onChange={(e) => setName(e.target.value)}
+							/>
+							<input
+								type="text"
+								className="outline-none border-none bg-lightBackground text-white font-nunito placeholder:text-opacity-60 focus:text-opacity-100 px-5 py-2 w-full rounded-full"
+								defaultValue={email}
+								onChange={(e) => setEmail(e.target.value)}
+							/>
+							<p className={`font-nunito text-red-500 ${error.length !== 0 ? 'mt-3' : '`'}`}>{error}</p>
+							<button
+								className="bg-lightBackground font-montserrat text-accent2 hover:bg-accent2 hover:text-middleBackground transition-colors duration-300 px-6 py-2 mt-2 max-w-fit rounded-xl cursor-pointer"
+								onClick={updateUser}
+							>
+								Actualizează
+							</button>
+						</form>
+					</div>
+				</div>
+			)}
+
+			<div className="min-w-full min-h-screen bg-background flex flex-col xl:items-center">
+				<Header isLoggedIn={isSignedIn} />
+				<div className="flex flex-col items-center md:mt-8 p-8 max-w-md md:max-w-full xl:max-w-6xl">
+					<Image
+						src={
+							user.photoURL
+								? user.photoURL
+								: `https://ui-avatars.com/api/?background=156271&color=27FB6B&?size=150&name=${
+										user.displayName.split(" ")[0]
+								  }+${
+										user.displayName.split(" ")[1] || ""
+								  }&bold=true&format=svg`
+						}
+						alt={"Profile picture"}
+						width={150}
+						height={150}
+						className="rounded-full"
+					/>
+					<p className="font-montserrat text-white text-2xl mt-6 mb-1">
+						{user.displayName}
+					</p>
+					<p className="font-nunito text-white opacity-50">{user.email}</p>
+					<div className="flex flex-col gap-6 mt-10">
+						<button
+							onClick={() => setPopupVisibility(!popupVisibility)}
+							className="bg-lightBackground font-montserrat text-accent2 hover:bg-middleBackground transition-colors duration-300 px-6 py-2 rounded-full cursor-pointer"
+						>
+							Editează
+						</button>
+						<button
+							onClick={() => logOut()}
+							className="bg-red-600 font-montserrat text-white hover:bg-red-800 transition-colors duration-300 px-6 py-2 rounded-full cursor-pointer"
+						>
+							Deconectează-te
+						</button>
+					</div>
+				</div>
+			</div>
+		</>
+	);
+};
+
+export default ProfilulMeu;
