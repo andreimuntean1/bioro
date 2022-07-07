6a3e24c3c96d06ab88d59142d68c40dd65e47167
Andrei Muntean <andreymuntean2004@gmail.com>
2022-07-07 17:04:46 +0300

    feat: header is dynamic based on user auth state

--- pages/[afacere].jsx
+++ pages/[afacere].jsx
@@ -3,15 +3,18 @@ import Image from "next/image";
 import Header from "components/Header";
 import { collection, doc, getDoc, getDocs, getFirestore } from "firebase/firestore";
 import { app } from "database";
+import { useAuth } from "hooks/useAuth";
 
 const Afacere = ({data}) => {  
+  const {pending, isSignedIn} = useAuth();
+
   return (
     <>
       <Head>
         <title>BioRO - {data.name}</title>
       </Head> 
       <div className="font-nunito min-w-full min-h-screen bg-background text-white flex flex-col xl:items-center md:max-w-full xl:max-w-3xl">
-				<Header isLoggedIn={false} />
+				<Header isLoggedIn={isSignedIn} />
         {/* <Image src={urlFor(data.cover_image).url()} alt={`Imagine de copertă pentru ${}`} className="w-full" /> */}
         <div className="p-8">
           <div className="mb-5">
